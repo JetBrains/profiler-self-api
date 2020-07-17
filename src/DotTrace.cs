89986cb2ee38e4d514fe3b6614b8e707c29ac6ed
Mikhail Pilin <mikhail@pilin.name>
2020-07-17 11:32:16 +0200

    Fix dotTrace error message

--- src/DotTrace.cs
+++ src/DotTrace.cs
@@ -169,7 +169,7 @@ namespace JetBrains.Profiler.SelfApi
             if (config == null) throw new ArgumentNullException(nameof(config));
 
             if(Environment.Version.Major == 3 && Environment.Version.Minor == 0)
-                throw new Exception("Self profiling api is not supported on .NET Core 3.0 or earlier");
+                throw new Exception("Self API is not supported on .NET Core 3.0");
 
             lock (OurMutex)
             {
