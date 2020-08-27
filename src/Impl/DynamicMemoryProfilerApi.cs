2cd31269d83fad4b69a73627d9a763097bb6284c
Mikhail Pilin <mikhail@pilin.name>
2020-08-27 14:28:45 +0200

    Switch to always using the profiler API package.

--- src/Impl/DynamicMemoryProfilerApi.cs
+++ src/Impl/DynamicMemoryProfilerApi.cs
@@ -1,63 +1,20 @@
-using System;
+using JetBrains.Profiler.Api;
 
 namespace JetBrains.Profiler.SelfApi.Impl
 {
   internal sealed class DynamicMemoryProfilerApi
   {
-    [Flags]
-    private enum MemoryFeatures : uint
+    private DynamicMemoryProfilerApi()
     {
-      Ready = 0x1,
-      Detach = 0x2,
-      CollectAllocations = 0x4
     }
 
-    private delegate void GetSnapshotDelegate(string name);
-
-    private delegate void DetachDelegate();
-
-    private delegate MemoryFeatures GetFeaturesDelegate();
-
-    private readonly GetSnapshotDelegate _getSnapshot;
-    private readonly DetachDelegate _detach;
-    private readonly GetFeaturesDelegate _getFeatures;
-
-    private DynamicMemoryProfilerApi(GetSnapshotDelegate snapshot, DetachDelegate detach, GetFeaturesDelegate getFeatures)
-    {
-      _getSnapshot = snapshot;
-      _detach = detach;
-      _getFeatures = getFeatures;
-    }
-
-    public void GetSnapshot(string name)
-    {
-      _getSnapshot(name);
-    }
-
-    public void Detach()
-    {
-      _detach();
-    }
+    public void GetSnapshot(string name) => MemoryProfiler.GetSnapshot(name);
+    public void Detach() => MemoryProfiler.Detach();
+    public bool IsReady() => (MemoryProfiler.GetFeatures() & MemoryFeatures.Ready) == MemoryFeatures.Ready;
 
     public static DynamicMemoryProfilerApi TryCreate()
     {
-      var profiler = DynamicProfiler.TryCreate("JetBrains.Profiler.Api.MemoryProfiler");
-
-      var getSnapshot = profiler?.TryGetMethod<GetSnapshotDelegate>("GetSnapshot", typeof(string));
-      if (getSnapshot == null)
-        return null;
-
-      var detach = profiler.TryGetMethod<DetachDelegate>("Detach");
-      if (detach == null)
-        return null;
-
-      var getFeatures = profiler.TryGetMethod<GetFeaturesDelegate>("GetFeatures");
-      if (getFeatures == null)
-        return null;
-
-      return new DynamicMemoryProfilerApi(getSnapshot, detach, getFeatures);
+      return new DynamicMemoryProfilerApi();
     }
-
-    public bool IsReady() => (_getFeatures() & MemoryFeatures.Ready) == MemoryFeatures.Ready;
   }
 }
