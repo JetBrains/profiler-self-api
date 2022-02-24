2babda2bfdab8075e1b41d8740b1d0dd3820a962
Mikhail Pilin <mikhail@pilin.name>
2022-02-24 17:00:18 +0300

    Switch to 2022.1

--- src/Impl/CommandLineToolsConfig.cs
+++ src/Impl/CommandLineToolsConfig.cs
@@ -6,6 +6,6 @@
     ///   The version (major and minor only!!!) of JetBrains.dotTrace.CommandLineTools and JetBrains.dotMemory.Console
     ///   NuGet-packages that must be downloaded.
     /// </summary>
-    public static readonly NuGet.SemanticVersion NupkgVersion = new(2021, 3);
+    public static readonly NuGet.SemanticVersion NupkgVersion = new(2022, 1);
   }
 }
