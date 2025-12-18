ef40b2c9bddf0bca9c5110e95315c2fb6323181e
Valentin Grigorev <valentin.grigorev@jetbrains.com>
2025-12-18 22:55:48 +0100

    Update CommandLineToolsConfig.NupkgVersion to 2025.3

--- JetBrains.Profiler.SelfApi/src/Impl/CommandLineToolsConfig.cs
+++ JetBrains.Profiler.SelfApi/src/Impl/CommandLineToolsConfig.cs
@@ -5,7 +5,7 @@
     // The version (major and minor only!!!) of JetBrains.dotTrace.CommandLineTools and JetBrains.dotMemory.Console
     // NuGet-packages that must be downloaded.
     // Don't forget to update xmldoc. The xmldoc is used for injecting proper version into Init methods (inheritdoc).
-    /// <summary>2023.1</summary>
-    internal static readonly NuGet.SemanticVersion NupkgVersion = new(2024, 1);
+    /// <summary>2025.3</summary>
+    internal static readonly NuGet.SemanticVersion NupkgVersion = new(2025, 3);
   }
 }
